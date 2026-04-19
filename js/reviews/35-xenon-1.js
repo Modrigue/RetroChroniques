e475e629321f49568df781a62be13bbb8413ade8
Nico <thealdobaldo@gmail.com>
2026-04-20 00:19:46 +0200

    add: added review

---
+++ js/reviews/35-xenon-1.js
@@ -0,0 +1,26 @@
+window.CHRONIQUES.push({
+  id: 35,
+  title: "Xenon",
+  platforms: ["Atari ST", "Amiga"],
+  rating: 2.5,
+  year: 1988,
+  pubDate: "Sun, 19 Apr 2026 22:17:00 GMT",
+  category: "standard",
+  nbPlayers: 1,
+  review: [
+    "On a tendance à l'oublier, mais avant <em>Xenon 2</em>, il y a  eu... <em>Xenon 1</em> ! Et le moins que l'on puisse dire, pour le premier jeu des Bitmap Brothers, c'est que... c'est pas encore ça... Dans le sens où, habituellement, ils réussissent à se frotter à plusieurs styles de jeux bien établis tout en y apportant leur propre patte, et des innovations par-dessus le marché.",
+    "Là, pas grand chose à se mettre sous la dent. Mis à part la petite voix digitalisée d'un développeur en début de niveau, la seule originalité du jeu consiste à pouvoir transformer son vaisseau en tank terrestre, roulant dans toutes les directions. Le problème est que l'idée n'est pas vraiment exploitée. De plus, la métamorphose n'est possible que dans 2 des seuls 4 niveaux.",
+    "Niveaux graphismes ça manque cruellement de personnalité, certains boss sont même rebondants. Bref, ce n'est même pas un autobus de différence qu'il y a entre les deux Xenon, mais carrément le vaisseau spatial des SpaceBalls ! Alors oui, il y a un petit peu d'armement, mais pas de magasin, et non pas 1 mais 2 musiques durant le jeu (3 avec celle des boss).",
+    "Heureusement que pour eux la valeur n'attend point le nombre des années. Après ce premier coup d'épée dans l'eau, les frères Bitmap enchaîneront ensuite des tubes vidéoludiques..."
+  ],
+  images: [
+    { src: "img/Xenon1_1.jpg", alt: "Xenon - Screenshot 1" },
+    { src: "img/Xenon1_2.jpg", alt: "Xenon - Screenshot 2" }
+  ],
+  imagesAfterParagraph: 2,
+  links: [],
+  bonusRatings: [
+    { id: 0 },
+    { id: 34 }
+  ]
+});
