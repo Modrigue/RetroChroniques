655d40f4f462d8027aee6a61a39788f40e8767d4
Modrigue <Modrigue@users.noreply.github.com>
2026-04-19 10:14:34 +0000

    deploy: d1a7237517976a8823f296e3cdfc18247f55f112

---
+++ js/reviews/34-xenon-2-megablast.js
@@ -0,0 +1,27 @@
+window.CHRONIQUES.push({
+  id: 34,
+  title: "Xenon 2 Megablast",
+  platforms: ["Atari ST", "Amiga", "PC"],
+  rating: 4.5,
+  year: 1989,
+  pubDate: "Sun, 19 Apr 2026 10:13:00 GMT",
+  category: "standard",
+  nbPlayers: 1,
+  review: [
+    "Fraîchement déballé sous le sapin de Noël 1989, Xenon 2 fut le deuxième jeu vidéo auquel j'ai joué sur mon Atari ST flambant neuf, ainsi que ma première gifle vidéoludique. Dans le sens où le jeu vidéo se donnait vraiment les moyens de ses ambitions et franchissait un cap. Du haut de mes trois pommes en tout cas.",
+    "Véritable super-production de l'époque, la claque commence avec le morceau <em>Megablast</em> de <em>Bomb The Bass</em>, remix hip-hop/rock du morceau titre électro du film <em>Assault</em> de John Carpenter. Croyez-moi, la première fois, ça vous prend aux tripes. D'autant plus que la version Atari ST envoie plus le bois - pour une fois - que la version Amiga, plus léchée.",
+    "Après le choc sonore, le choc visuel : Non mais regardez-moi ces décors de toute beauté ! Le détail et le rendu des graphismes met la misère aux autres shmups de l'époque. Quel progrès par rapport à Xenon premier du nom, seulement sorti un an auparavant ! L'action se déroule à plusieurs ères (crétacé, jurassique, etc....), sur 5 niveaux, chacun découpé en 2 parties, avec des boss à la clé. Et quels boss! Certains peuvent prendre plusieurs écrans, du jamais vu à l'époque pour un shoot 'em up vertical.",
+    "Cependant, le jeu ne se contente pas d'être une prouesse technique et propose moultes innovations. La plus importante à mon goût est celle du magasin. Une authentique caverne d'Ali Baba où vous allez pouvoir, 2 fois par niveau, construire votre vaisseau à la carte. Si vous commencez le jeu avec le piou-piou de base, par la suite, c'est une véritable armada, une puissance de feu titanesque, que vous allez diriger... Si vous vous débrouillez bien avec votre argent.",
+    "Car non seulement vous allez devoir réfléchir à ce que vous achetez, mais aussi à ce que vous vendez ! Un armement pourra très bien être adapté à une configuration d'un niveau et pas du tout à un autre. Cette gestion poussée de l'équipement, à des années-lumière de ce qui se faisait à l'époque dans les shmups, offre une grande replay value. Une telle customisation de vaisseau ne sera surpassée, à ma connaissance, que plus tard par Tyrian.",
+    "Autre spécificité du jeu : bien que le scrolling soit vertical, il est possible de faire reculer le vaisseau pour aller en arrière et emprunter un autre passage, ou freiner pour éviter un projectile. Il y a carrément une section sous forme de mini-labyrinthe !",
+    "Paradoxalement, le seul défaut de Xenon 2 est un de ses points forts : sa bande originale, toujours le même unique morceau malheureusement, quelque soit le niveau. Et aussi sa fin, où l'on doit bêtement recommencer le jeu avec les ennemis plus résistants. Une coutume dans les jeux d'arcade des années 80, mais qui n'avait plus vraiment sa place pour un jeu de cette ambition qui inaugurait une nouvelle décennie.",
+    "Aux antipodes des manic shooters style ThunderForce et autres DonDonpachi, Xenon 2 se rapprocherait plus de R-Type en termes de philosophie d'action. Un shoot 'em up posé, où l'on prend son temps, on réfléchit, on observe, on apprend, avant de tirer sur tout ce qui bouge avec l'équipement qu'on aura soigneusement choisi. À découvrir d'urgence, sur Amiga ou Atari ST, pour faire votre propre expérience, si vous êtes fan du genre."
+  ],
+  images: [
+    { src: "img/Xenon2_1.jpg", alt: "Xenon 2 Megablast - Screenshot 1" },
+    { src: "img/Xenon2_2.jpg", alt: "Xenon 2 Megablast - Screenshot 2" }
+  ],
+  imagesAfterParagraph: 3,
+  links: [{ url: "https://www.youtube.com/watch?v=CKM3TSLXh4g", type: "youtube", label: "Vidéo YouTube" }],
+  bonusRatings: null
+});
