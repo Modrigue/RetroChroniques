c6f8d04acc4ec48f3c38797b510a1d826507d268
Modrigue <thealdobaldo@gmail.com>
2026-04-20 17:43:56 +0200

    add: added review

---
+++ js/reviews/36-batman-the-movie.js
@@ -0,0 +1,24 @@
+window.CHRONIQUES.push({
+  id: 36,
+  title: "Batman The Movie",
+  platforms: ["Amiga", "Atari ST"],
+  rating: 3.5,
+  year: 1989,
+  pubDate: "Mon, 20 Apr 2026 18:42:00 GMT",
+  category: "standard",
+  nbPlayers: 1,
+  review: [
+    "Tout simplement le premier jeu vidéo auquel j'ai joué chez moi ce Noël 1989. Et cette rencontre vidéoludique fut autant bouleversifiante... qu'exigeante, avec sans doute l'un des premiers niveaux les plus difficiles de l'histoire. Ah, ça, de mon temps, les jeux vidéo ça vous apprenait la rigueur, la patience, la discipline, pas comme les jeux d'aujourd'hui hein. C'est à la sueur de son front qu'on devenait un as du jeu de plate-formes... ou pas.",
+    "Car pour un premier niveau de plate-formes - et c'est ça qui est drôle - Batman ne peut pas sauter. Oh non, là, il faudra utiliser le grappin, pardon la <em>Batrope</em>, un peu à la manière de <em>Bionic Commando</em>, tout en faisant TRÈS attention aux ennemis qui vous balancent joyeusement leurs projectiles et autres grenades en pleine poire.",
+    "Pas question de foncer tête baissée avec vos <em>Batarangs</em>, vous allez vous prendre une saucée et adieu Kim Basinger. Dans l'approche, ce serait presque un niveau d'infiltration... si ce n'était pour la maniabilité de Batman, aussi agile qu'un 36 tonnes. Il va falloir vous armer de patience et surtout de rigueur pour passer ce niveau de l'usine et vous enjailler sur la suite du jeu, bien plus fun (et plus simple !).",
+    "Voyez plutôt : vous pourrez diriger la <em>BatMobile</em>, le <em>Batwing</em> et identifier les produits du Joker dans la <em>BatCave</em>. Les niveaux avec les véhicules sont très réussis et ceux sur lesquels je me suis le plus amusé, notamment avec le grappin de la <em>Batmobile</em>. La fin se déroule dans la cathédrale et reprend la structure de plateforme utilisée au premier niveau. Donc... bonne chance. Tout ça à l'époque sans sauvegarde, bien entendu.",
+    "Alors que la plupart des jeux à licence était pour la plupart ratés, <em>Batman The Movie</em> suit assez fidèlement le film, avec une réalisation technique et une variété de gameplays fort convaincantes. Bien que la maniabilité gagnerait à être revue dans les niveaux de plateforme, cet opus moins connu est à essayer pour les fans (courageux) de la chauve-souris."
+  ],
+  images: [
+    { src: "img/BatmanTheMovie_1.jpg", alt: "Batman The Movie - Screenshot 1" },
+    { src: "img/BatmanTheMovie_2.jpg", alt: "Batman The Movie - Screenshot 2" }
+  ],
+  imagesAfterParagraph: 2,
+  links: [],
+  bonusRatings: null
+});
