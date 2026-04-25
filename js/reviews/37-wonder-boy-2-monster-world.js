3257be8ff32f9c6db2083a707624160730333a67
Modrigue <Modrigue@users.noreply.github.com>
2026-04-25 13:30:59 +0000

    deploy: 587981a74fb249d198a5a7a83d7121a047b6ca45

---
+++ js/reviews/37-wonder-boy-2-monster-world.js
@@ -0,0 +1,23 @@
+window.CHRONIQUES.push({
+  id: 37,
+  title: "Wonderboy 2 / Monster World",
+  platforms: ["Amiga", "Arcade", "Atari ST", "Master System"],
+  rating: 4,
+  year: 1987,
+  category: "standard",
+  nbPlayers: 1,
+  review: [
+    ""
+  ],
+  images: [
+    { src: "img/Wonderboy2_1.jpg", alt: "Wonder Boy 2 - Screenshot 1" },
+    { src: "img/Wonderboy2_2.jpg", alt: "Wonder Boy 2 - Screenshot 2" }
+  ],
+  links: [{ url: "https://www.youtube.com/watch?v=02D4-mvmi9c", type: "youtube", label: "Vidéo du jeu" }],
+  bonusRatings: [
+    { id: 0 },
+    { id: 4 },
+    { id: 22 },
+    { id: 15 }
+  ]
+});
